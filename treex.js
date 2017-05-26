2e2c357e8db92b2daba208e0fdf4aba1c4a4094e
User13 <User13>
2017-05-26 22:15:34 +0300

    major assembling mechanism

---
+++ treex.js
@@ -0,0 +1,95 @@
+// ==UserScript==
+// @name         DOMJSON
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://vk.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    // Your code here...
+    'use strict';
+    var DOMJSON = function(){
+    	console.log(document);
+    };
+
+    DOMJSON.prototype = {
+    	format : function(value, format){
+    		format = format.split('>');
+    		var type = format[0];
+    		var action = format[1];
+
+    		if (typeof value == type){
+    			switch(action){
+    				case 'rm-spaces':
+    					return value.replace(/^\s+|\s+$/g,'');
+    				break;
+    				case 'parse-number':
+    					return Number(value) || 0;
+    				break;
+    				default:
+    					return value;
+    				break;
+    			}
+    		}
+
+    		return value;
+
+    	},
+    	asm : function(description, parent, el){
+    		parent = parent || window.document;
+
+    		var value;
+
+    		if (typeof el == 'undefined'){
+    			if (typeof description.selector == 'string'){
+    				var elements = parent.querySelectorAll(description.selector);
+
+    				if (elements.length == 0){
+    					value = null;
+    				} else if ((elements.length == 1 && description.array !== true) || description.array === false){
+	    				el = elements[0];
+	    			} else {
+	    				value = [];
+
+	    				for (var a = 0, l = elements.length; a < l; a++){
+	    					value.push(this.asm(description, parent, elements[a]));
+	    				}
+	    			}
+
+    			} else {
+    				el = parent;
+    			}
+    		}
+
+    		if (typeof value == 'undefined'){
+    			switch(description.type){
+	    			case 'node-html':
+	    				value = el.innerHTML;
+	    			break;
+	    			case 'branch':
+	    				value = {};
+
+	    				for (var k in description.value){
+	    					value[k] = this.asm(description.value[k], el);
+	    				}
+	    			break;
+	    		}
+    		}
+
+    		if (description.format){
+    			for (var b = 0, l = description.format.length; b < l; b++){
+    				value = this.format(value, description.format[b]);
+    			}
+    		}
+
+    		return value;
+    		
+    	},
+    };
+
+    window.domJSON = new DOMJSON();
+
+})();
